38097e4be9bda2df5b6e17620e7e0be8af878182
Dinesh Padisetti <dinesh.padisetti@gmail.com>
2016-09-06 00:15:15 +0530

    Added the main api paths

--- server.py
+++ server.py
@@ -1,9 +1,19 @@
 from flask import Flask, jsonify, request
+import json
 app = Flask(__name__)
 
 @app.route("/search")
-def hello():
-    return jsonify({'working' : request.args.get('q')})
+def search():
+    query = request.args.get('q')
+    return json.dumps([{'urlResult' : 'http://google.com/',
+                        'textResult' : query }])
+
+@app.route("/url", methods=['POST'])
+def url():
+    if request.method == 'POST':
+        content = request.get_json(silent=True)
+        url = content['url']
+        return json.dumps({'url': url, 'indexed': True})
 
 if __name__=="__main__":
     app.run(debug=True)
