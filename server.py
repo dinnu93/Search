fb587ae3853e7e8aa0bb7d240708b1ed58dcc1e6
Dinesh Padisetti <dinesh.padisetti@gmail.com>
2016-09-06 02:36:11 +0530

    Added some stuff

--- server.py
+++ server.py
@@ -12,9 +12,12 @@ def search():
 def url():
     if request.method == 'POST':
         content = request.get_json(silent=True)
-        url = content['url']
-        return json.dumps({'url': url, 'indexed': True})
-
+        if 'url' in content:
+            url = content['url']
+            return json.dumps({'url': url, 'indexed': True})
+        else:
+            return json.dumps({'error': 'Not a valid request body!'})
+        
 if __name__=="__main__":
     app.run(debug=True)
     
