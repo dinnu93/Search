116d097ba2271a397a6bf062dd39b0c4625d9a49
dinnu93 <dinesh.padisetti@gmail.com>
2016-09-13 23:17:42 +0000

    Made some changes

--- clean.py
+++ clean.py
@@ -5,17 +5,12 @@ from bs4 import BeautifulSoup
 def htmlToText(url):
     html = urllib2.urlopen(url).read()
     soup = BeautifulSoup(html, "html.parser")
-    # kill all script and style elements
     for script in soup(["script", "style"]):
         script.extract()    # rip it out
-        # get text
     text = soup.get_text()
-    # break into lines and remove leading and trailing space on each
     lines = (line.strip() for line in text.splitlines())
-    # break multi-headlines into a line each
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    # drop blank lines
     text = ' '.join(chunk for chunk in chunks if chunk)
-    text = re.sub("[^a-zA-Z0-9\ ]+", "", text)
-    return text
+    # text = re.sub("[^a-zA-Z0-9\ ]+", "", text) 
+    return text.encode('utf-8')
                                                                     
